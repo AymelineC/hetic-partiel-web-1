f0889f37b63c182cbbf21741cb24ba94a03fa78a
AymelineC <aymelinech@gmail.com>
2018-06-14 12:42:15 +0200

    Fixes #01 - Burger Menu

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var openButton = document.querySelector('.burger');
+var closeButton = document.querySelector('.cross');
+var mainavUl =document.querySelector('#mainav');
+
+openButton.addEventListener("click", function() {
+    mainavUl.style.display = "block";
+    openButton.style.display = "none";
+    closeButton.style.display = "block";
+    console.log('1');
+
+
+})
+
+closeButton.addEventListener("click", function() {
+    mainavUl.style.display = "none";
+    openButton.style.display = "block";
+    closeButton.style.display = "none";
+    console.log('2');
+
+
+})
